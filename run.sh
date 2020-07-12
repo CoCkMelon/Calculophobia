3a72b62bf369a7cbf5a90fa4a572887b9a08e6e5
CoCkMelon <ha@ha.ha>
2020-07-12 02:54:36 +0000

    fix scripts

--- run.sh
+++ run.sh
@@ -3,7 +3,18 @@ cd $(dirname $0)/vnka
 if ! test -f ./Calculophobia ; then
 	./compile
 fi
-if ! test -f shaders/spv/slide0.frag.spv ; then
-	./shaders/build_shaders.sh
+cd shaders/src
+if ! test -f ../spv/main.vert.spv ; then
+	glslangValidator -V100 main.vert -o ../spv/main.vert.spv
 fi
+if ! test -f ../spv/main.frag.spv ; then
+	glslangValidator -V100 main.frag -o ../spv/main.frag.spv
+fi
+for n in slide*.frag
+do 
+	if ! test -f ../spv/"$n".spv ; then
+		glslangValidator -V100 "$n" -o ../spv/"$n".spv
+	fi
+done
+cd ../../
 exec ./Calculophobia
